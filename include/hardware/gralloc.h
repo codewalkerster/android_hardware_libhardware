cdf47380bf87a1783a911c98a976d44a78cda921
codewalker <codewalker@hardkernel.com>
2014-07-10 20:22:18 +0900

    5410 4.2.2 master
    Change-Id: I0912f034f8ab325397351804e30ed1606ec2ae6d

--- include/hardware/gralloc.h
+++ include/hardware/gralloc.h
@@ -84,8 +84,10 @@ enum {
     GRALLOC_USAGE_HW_CAMERA_ZSL         = 0x00060000,
     /* mask for the camera access values */
     GRALLOC_USAGE_HW_CAMERA_MASK        = 0x00060000,
+    /* buffer will be used by the framebuffer device with sysmmu off */
+    GRALLOC_USAGE_HW_FB_PHY_LINEAR      = 0x00008000,
     /* mask for the software usage bit-mask */
-    GRALLOC_USAGE_HW_MASK               = 0x00071F00,
+    GRALLOC_USAGE_HW_MASK               = 0x00079F00,
 
     /* buffer should be displayed full-screen on an external display when
      * possible
@@ -106,6 +108,13 @@ enum {
     GRALLOC_USAGE_PRIVATE_2             = 0x40000000,
     GRALLOC_USAGE_PRIVATE_3             = 0x80000000,
     GRALLOC_USAGE_PRIVATE_MASK          = 0xF0000000,
+
+    GRALLOC_USAGE_INTERNAL_ONLY         = 0x10000000,
+    GRALLOC_USAGE_EXTERNAL_FLEXIBLE     = 0x20000000,
+    GRALLOC_USAGE_EXTERNAL_BLOCK        = 0x40000000,
+    GRALLOC_USAGE_EXTERNAL_ONLY         = 0x80000000,
+    GRALLOC_USAGE_EXTERNAL_VIRTUALFB    = 0x00400000,
+    GRALLOC_USAGE_PRIVATE_NONSECURE     = 0x02000000,
 };
 
 /*****************************************************************************/
