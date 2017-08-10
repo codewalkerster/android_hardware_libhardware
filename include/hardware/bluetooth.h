c5cf7cbfc14472fdb8dd411684462b6988b0e01c
Luke Go <luke.go@hardkernel.com>
2017-08-10 13:50:27 +0900

    Revert "bluetooth: support realtek bt"
    This reverts commit c8e115e977389baed99e34187ebb7fc33c2ba960.
    
    Change-Id: I6f15dd17079bcc841f68c8ddb88708bb8ed4e1ac

--- include/hardware/bluetooth.h
+++ include/hardware/bluetooth.h
@@ -33,8 +33,7 @@ __BEGIN_DECLS
 #define BT_HARDWARE_MODULE_ID "bluetooth"
 #define BT_STACK_MODULE_ID "bluetooth"
 #define BT_STACK_TEST_MODULE_ID "bluetooth_test"
-#define BT_HARDWARE_RTK_MODULE_ID "bluetooth_rtk"
-#define BT_STACK_RTK_MODULE_ID "bluetooth_rtk"
+
 
 /* Bluetooth profile interface IDs */
 
@@ -48,9 +47,6 @@ __BEGIN_DECLS
 #define BT_PROFILE_PAN_ID "pan"
 #define BT_PROFILE_MAP_CLIENT_ID "map_client"
 #define BT_PROFILE_SDP_CLIENT_ID "sdp"
-#ifdef BLUETOOTH_RTK_API
-#define BT_PROFILE_RTKBT_ID "rtkbt"
-#endif
 #define BT_PROFILE_GATT_ID "gatt"
 #define BT_PROFILE_AV_RC_ID "avrcp"
 #define BT_PROFILE_AV_RC_CTRL_ID "avrcp_ctrl"
